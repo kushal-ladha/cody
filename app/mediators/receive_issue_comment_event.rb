b11fce715e7989487b191dbfe023ad63a628357b
Chris Fung <aergonaut@gmail.com>
2019-11-05 15:50:25 -0800

    Handle nil review rule in replace me (#359)
    If a reviewer requests to be replaced but they were not assigned via a
    review rule, then we would attempt to find possible replacements and hit
    a `nil`.
    
    This commit handles that case by skipping over review assignments if
    they did not come from a review rule.

--- app/mediators/receive_issue_comment_event.rb
+++ app/mediators/receive_issue_comment_event.rb
@@ -194,6 +194,7 @@ class ReceiveIssueCommentEvent
     return unless old_reviewers.present?
 
     old_reviewers.each do |reviewer|
+      next unless reviewer.review_rule.present?
       pos_logins = reviewer.review_rule.filtered_reviewers(pr)
       new_usr = pos_logins.reject { |usr| usr.casecmp(commenter).zero? }.sample
       reviewer.update!(login: new_usr) if new_usr
